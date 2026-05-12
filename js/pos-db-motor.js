7138463fff084106d4f016e4f12c014e530692a4
sistematikos <f4anks@gmail.com>
2026-05-12 11:11:58 -0400

    Update pos-db-motor.js

--- js/pos-db-motor.js
+++ js/pos-db-motor.js
@@ -5,6 +5,7 @@ const UID = "sUhfZI9Fy3M9UlInTYw2wFWZmB12";
 export let tasaActual = 0;
 export let productosMaster = [];
 
+// Escucha Realtime de Tasa
 onSnapshot(doc(db, "usuarios", UID, "configuracion", "tasa"), (s) => {
     if (s.exists()) {
         tasaActual = s.data().valor;
@@ -13,12 +14,14 @@ onSnapshot(doc(db, "usuarios", UID, "configuracion", "tasa"), (s) => {
     }
 });
 
+// Escucha Realtime de Inventario
 onSnapshot(collection(db, "usuarios", UID, "productos"), (s) => {
     productosMaster = [];
     s.forEach(d => productosMaster.push({ id: d.id, ...d.data() }));
     document.dispatchEvent(new CustomEvent('productosActualizados'));
 });
 
+// Proceso de Venta
 export async function procesarVentaFirebase(carrito, total, metodo) {
     try {
         await addDoc(collection(db, "usuarios", UID, "ventas"), {
@@ -29,8 +32,13 @@ export async function procesarVentaFirebase(carrito, total, metodo) {
             tasa: tasaActual
         });
         for (let item of carrito) {
-            await updateDoc(doc(db, "usuarios", UID, "productos", item.id), { stock: increment(-item.cantidad) });
+            await updateDoc(doc(db, "usuarios", UID, "productos", item.id), { 
+                stock: increment(-item.cantidad) 
+            });
         }
         return true;
-    } catch (e) { return false; }
+    } catch (e) { 
+        console.error("Error DB:", e);
+        return false; 
+    }
 }
