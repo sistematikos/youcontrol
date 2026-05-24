c9674a3e401dd98f704df1c047be00d98dab3a17
sistematikos <f4anks@gmail.com>
2026-05-24 16:17:00 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,19 @@
+// Importamos las funciones necesarias usando la misma versión de CDN que en pos-core.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+// Configuración de tu proyecto
 const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "youcontrol-1d60a.firebaseapp.com",
-    projectId: "youcontrol-1d60a",
-    storageBucket: "youcontrol-1d60a.appspot.com",
-    messagingSenderId: "TU_SENDER_ID",
-    appId: "TU_APP_ID"
+  apiKey: "AIzaSyAMkpj9tFps526mL6hO-ciePWrTdx_NK9Q",
+  authDomain: "youcontrol-1d60a.firebaseapp.com",
+  projectId: "youcontrol-1d60a",
+  storageBucket: "youcontrol-1d60a.firebasestorage.app",
+  messagingSenderId: "812100760013",
+  appId: "1:812100760013:web:7574906aa285555faf5484"
 };
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-// Exportar la base de datos para usarla en otros archivos
-export const db = getFirestore(app);
 
+// EXPORTAR la base de datos para que pos-core.js pueda leerla
+export const db = getFirestore(app);
