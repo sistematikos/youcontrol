880a25ad329a3961ec48b8c627ad764c1cd5a134
sistematikos <f4anks@gmail.com>
2026-06-08 18:57:40 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,25 @@
 const CACHE_NAME = 'youcontrol-cache-v1';
-const urlsToCache = ['./index.html', './sys_lic.html'];
+const urlsToCache = [
+  './index.html',
+  './sys_v1_menu.html', // Asegúrate de incluir tus archivos principales aquí
+  './manifest.json',
+  './assets/logo_512.png'
+];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Fuerza la instalación inmediata
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
   );
 });
 
